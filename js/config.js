21ae16a4e4b33f5f43ce2394406993f8f1f278b1
Bhajan Dhami <bhajandhami314@gmail.com>
2022-06-18 22:25:53 +0530

    update config file

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_Lilck9217', // e.g. ap-south-1_Lilck9217
+        userPoolClientId: '5h50ajlur9002ol5le85h50fv0', // e.g. 5h50ajlur9002ol5le85h50fv0
+        region: 'ap-south-1' // e.g. ap-south-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
